b400dcb4914782e41aec9f08d83fd31d10f132e8
tomomi mizukami <ff10mm11yy23tamk@gmail.com>
2025-08-01 07:29:00 +0900

    styled-componentsを使用してスタイルを適用

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,46 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Heading from "./ui/Heading";
+
+const Button = styled.button`
+  font-size: 1.4rem;
+  padding: 1.2rem 1.6rem;
+  font-weight: 500;
+  border: none;
+  border-radius: var(--border-radius-sm);
+  background-color: var(--color-brand-600);
+  color: var(--color-brand-50);
+  box-shadow: var(--shadow-sm);
+  cursor: pointer;
+  margin: 20px;
+`;
+
+const Input = styled.input`
+  border: 1px solid var(--color-grey-300);
+  background-color: var(--color-grey-0);
+  border-radius: var(--border-radius-sm);
+  padding: 0.8rem 1.2rem;
+`;
+
+const StyledApp = styled.main`
+  background-color: orangered;
+  padding: 20px;
+`;
+
 function App() {
-  return <div>Hello world</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <Heading as="h1">The Wild Oasis</Heading>
+        <Heading as="h2">The Wild Oasis</Heading>
+        <Button onClick={() => alert("チェックイン")}>Check in</Button>
+        <Button onClick={() => alert("チェックアウト")}>Check out</Button>
+        <Heading as="h3">The Wild Oasis</Heading>
+        <Input type="number" placeholder="利用者数" />
+      </StyledApp>
+    </>
+  );
 }
 
 export default App;
