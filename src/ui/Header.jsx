30da32651989b71af0d0fd48aa9ad249bff4b341
tomomi mizukami <ff10mm11yy23tamk@gmail.com>
2025-08-02 17:23:38 +0900

    AppLayout、Header、Sidebarコンポーネントを追加し、Appコンポーネントにレイアウトを適用

---
+++ src/ui/Header.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const StyledHeader = styled.header`
+  /* background-color: var(--color-grey-0); */
+  background-color: blanchedalmond;
+  padding: 1.2rem 4.8rem;
+  border-bottom: 1px solid var(--color-grey-100);
+`;
+
+function Header() {
+  return <StyledHeader>ヘッダー</StyledHeader>;
+}
+
+export default Header;
