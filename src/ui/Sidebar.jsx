528827c0dac9d369c3fdddf5243829ca91aeebab
tomomi mizukami <ff10mm11yy23tamk@gmail.com>
2025-08-02 23:11:40 +0900

    react-iconsを追加し、MainNavコンポーネントを作成してSidebarに統合

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import MainNav from "./MainNav";
+import Logo from "./Logo";
 
 const StyledSidebar = styled.aside`
   /* background-color: var(--color-grey-0); */
@@ -6,10 +8,18 @@ const StyledSidebar = styled.aside`
   padding: 3.2rem 2.4rem;
   border-right: 1px solid var(--color-grey-100);
   grid-row: 1 / -1;
+  display: flex;
+  flex-direction: column;
+  gap: 3.2rem;
 `;
 
 function Sidebar() {
-  return <StyledSidebar>サイドバー</StyledSidebar>;
+  return (
+    <StyledSidebar>
+      <Logo />
+      <MainNav />
+    </StyledSidebar>
+  );
 }
 
 export default Sidebar;
