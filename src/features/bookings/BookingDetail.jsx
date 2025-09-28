5502c71a1de0b79aff787b4de9c3bcf8746aa644
tomomi mizukami <ff10mm11yy23tamk@gmail.com>
2025-09-28 10:09:27 +0900

    feat: 予約詳細のローディング時にスピナーと空状態を表示

--- src/features/bookings/BookingDetail.jsx
+++ src/features/bookings/BookingDetail.jsx
@@ -34,6 +34,7 @@ function BookingDetail() {
   const navigate = useNavigate();
 
   if (isLoading) return <Spinner />;
+  if (!booking) return <Empty resourceName="booking" />;
 
   const statusToTagName = {
     unconfirmed: "blue",
